ddce00e721e16bf4d150616159652bec79cd4166
ritikramuka <ritik.ramuka@gmail.com>
2021-07-28 15:13:35 +0530

    update

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,32 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Card } from 'react-bootstrap'
+import { Card, Col, Row } from 'react-bootstrap'
 import Rating from './Rating'
 import './Style/Product.css'
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 px-3 pt-3 rounded productCard'>
+    <Card className='my-3 p-3 rounded productCard'>
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </Link>
 
-      <Card.Body>
-        <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
-            <strong>{product.name}</strong>
-          </Card.Title>
-        </Link>
+      <Link to={`/product/${product._id}`}>
+        <Card.Title as='div' className='mt-3'>
+          <strong>{product.name}</strong>
+        </Card.Title>
+      </Link>
 
-        <Card.Text as='div'>
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
-
-        <Card.Text as='h3'>${product.price}</Card.Text>
-      </Card.Body>
+      <Row>
+        <Col xs={4} className='pr-0'>
+          <Card.Text as='div'>${product.price}</Card.Text>
+        </Col>
+        <Col xs={8}>
+          <Card.Text as='small'>
+            <Row >
+              <Col xs={4} className='px-0 ratingNum'>
+                ({product.rating}/5)
+              </Col>
+              <Col xs={8} className='px-0'>
+                <Rating
+                  value={product.rating}
+                />
+              </Col>
+            </Row>
+          </Card.Text>
+        </Col>
+      </Row>
     </Card>
   )
 }
